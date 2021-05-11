e74db8b15e8933109fdb52e982ae231f70c19be9
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-11 18:31:18 -0400

    working on indexedDB

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import { useQuery } from '@apollo/react-hooks';
-
 import ProductItem from "../ProductItem";
-import { QUERY_PRODUCTS } from "../../utils/queries";
-import spinner from "../../assets/spinner.gif"
 import { useStoreContext } from "../../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../../utils/actions";
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_PRODUCTS } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers";
+import spinner from "../../assets/spinner.gif"
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
@@ -18,13 +18,24 @@ function ProductList() {
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+        data.products.forEach((product) => {
+          idbPromise('products', 'put', product);
+        });
+    } else if (!loading) {
+      idbPromise('products', 'get').then((products) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
+      });
     }
-  }, [data, dispatch])
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
       return state.products;
     }
+
     return state.products.filter(product => product.category._id === currentCategory);
   }
 
