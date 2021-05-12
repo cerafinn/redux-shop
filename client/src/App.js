b6177c43c9e7a27f8d0bd6c31409c7547f395f0b
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-11 22:16:58 -0400

    working on redux refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,16 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 import { Provider } from 'react-redux';
 
+import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Nav from "./components/Nav";
 import OrderHistory from "./pages/OrderHistory";
 import Success from "./pages/Success";
 
-import store from './utils/GlobalState';
+import store from './utils/store';
 
 const client = new ApolloClient({
   request: (operation) => {
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,3 @@
-// import { useReducer } from 'react';
 import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
@@ -82,9 +81,5 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
-
-// export function useProductReducer(initialState) {
-//   return useReducer(reducer, initialState);
-// };
 
 export default reducer;
